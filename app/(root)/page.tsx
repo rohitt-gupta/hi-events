58c4092551e8465254bd1a6c1e0bb74e541a4ea2
rohitGupta2022 <pta.rohit28@gmail.com>
2023-12-16 11:44:08 +0530

    Add: Clerk AUthentication and header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,8 +4,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <h1 className='text-4xl text-violet-600'>Hello</h1>
-      <Button variant='destructive' className='px-10'>DELETE</Button>
+
     </main>
   )
 }
